8216580c399466b6d10eb8c5a415650661f19aed
Benoit Marie <benoit.marie@lowatt.fr>
2025-08-01 21:57:12 +0200

    format files

--- js/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('scroll-to-products').addEventListener('click', () => {
-  // On récupère la section produits (il faut que tu aies un id="products" sur ta section)
-  const productsSection = document.getElementById('products');
-  if (productsSection) {
-    productsSection.scrollIntoView({ behavior: 'smooth' });
-  }
-});
